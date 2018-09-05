ff22810a8910fa4bc8a3de6983cce22d6951dd12
maxsvetlik <maxsvetlik@gmail.com>
2018-09-05 17:28:50 -0500

    Visemes for speech production (experimental, unsupported)

--- hlpr_dialogue_production/src/hlpr_dialogue_production/standalone_server.py
+++ hlpr_dialogue_production/src/hlpr_dialogue_production/standalone_server.py
@@ -72,9 +72,10 @@ class HLPRDialogueAction():
         controllers=[]
         controllers=[
             controller_gen.get_test_controller(),
-            controller_gen.get_lookat_controller(),
+            #controller_gen.get_lookat_controller(),
             #controller_gen.get_gesture_controller(),
             controller_gen.get_keyframe_playback_controller(),
+            controller_gen.get_viseme_controller(),
                     ]
         return controllers
 
